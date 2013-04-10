bf06d742678d72954fa15e8e1c2802be28582152
SipuliSopuli <SipuliSopuli@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-04-10 13:49:50 +0000

    tagging version 2.0
    git-svn-id: https://plugins.svn.wordpress.org/finnish-internet-black-day/tags/1.2@695279 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- mustapaiva.php
+++ mustapaiva.php
@@ -3,7 +3,7 @@
  * Plugin Name: Finnish Internet black day
  * Plugin URI: https://www.facebook.com/events/160986290729976/
  * Description: Use this plugin to add Finnish Internet black day code by SC5 for your site. You can install and enable plugin now, code is activated only on campaing day 23.4. 
- * Version: 1.0
+ * Version: 1.3
  * Author: Timi Wahalahti
  * Author URI: http://wahalahti.fi
  */
@@ -20,6 +20,8 @@ function mustapaiva_opt_init_fn(){
 	add_settings_field('title', 'Title:', 'setting_string_title', __FILE__, 'main_section');
 	add_settings_field('big_text', 'Big text:', 'setting_string_big_text', __FILE__, 'main_section');
 	add_settings_field('small_text', 'Small text:', 'setting_string_small_text', __FILE__, 'main_section');
+	add_settings_field('count_text', 'Signatures text<br><small>Text before signatures count</small>', 'setting_string_count_text', __FILE__, 'main_section');
+	add_settings_field('close_text', 'Close text', 'setting_string_close_text', __FILE__, 'main_section');
 	add_settings_field('coder', 'Show authors:<br><small>Be nice and show authors of code</small>', 'setting_string_coder', __FILE__, 'main_section');
 }
 
@@ -27,36 +29,38 @@ function mustapaiva_opt_add_page_fn() {
 	add_options_page('Finnish Internet black day', 'Finnish Internet black day', 'administrator', __FILE__, 'mustapaiva_opt_page_fn');
 }
 
-
 function  section_text_fn() {
 	echo "<p>By default code is active only in campaign day 23.4. and each visitor sees message only once.</p>";
 	echo '<p>If you want, you can modify texts in message.<br>You can use HTML-signs, for example link is added like this: &#60;a href="http://jarkea.fi/en" target="_blank"&#62;Common sense into copyright law&#60;/a&#62;. </p>';
 	echo "<p>If you modify texts, activate code for every day and check modifications.</p>";
 }
-
 function setting_string_only() {
 	$options = get_option('mustapaiva_options');
 	echo "<input type='checkbox' name='mustapaiva_options[only]'' value='1'" .checked( isset( $options['only'] ), true, false ). "/>";
 }
-
 function setting_string_once() {
 	$options = get_option('mustapaiva_options');
 	echo "<input type='checkbox' name='mustapaiva_options[once]'' value='1'" .checked( isset( $options['once'] ), true, false ). "/>";
 }
-
 function setting_string_title() {
 	$options = get_option('mustapaiva_options');
 	echo "<input id='title' name='mustapaiva_options[title]' size='40' type='text' value='{$options['title']}' />";
 }
-
 function setting_string_big_text() {
 	$options = get_option('mustapaiva_options');
 	echo "<input id='big_text' name='mustapaiva_options[big_text]' size='40' type='text' value='{$options['big_text']}' />";
 }
-
 function setting_string_small_text() {
 	$options = get_option('mustapaiva_options');
 	echo "<input id='small_text' name='mustapaiva_options[small_text]' size='40' type='text' value='{$options['small_text']}' />";
+}
+function setting_string_count_text() {
+	$options = get_option('mustapaiva_options');
+	echo "<input id='count_text' name='mustapaiva_options[count_text]' size='40' type='text' value='{$options['count_text']}' />";
+}
+function setting_string_close_text() {
+	$options = get_option('mustapaiva_options');
+	echo "<input id='close_text' name='mustapaiva_options[close_text]' size='40' type='text' value='{$options['close_text']}' />";
 }
 function setting_string_coder() {
 	$options = get_option('mustapaiva_options');
@@ -82,7 +86,9 @@ function mustapaiva_opt_page_fn() {
 function mustapaiva_options_validate($input) {
 	$input['title'] =  wp_kses($input['title']);
 	$input['big_text'] =  wp_kses($input['big_text']);
-	$input['small_text'] =  wp_kses($input['small_text']);	
+	$input['small_text'] =  wp_kses($input['small_text']);
+	$input['count_text'] =  wp_kses($input['count_text']);
+	$input['close_text'] =  wp_kses($input['close_text']);	
 	return $input;
 }
 
@@ -91,27 +97,32 @@ $options = get_option('mustapaiva_options');
 echo "<script type='text/javascript' src='" .plugins_url( 'mustapaiva.js' , __FILE__ ). "' charset='UTF-8'></script>";
 
 if (isset($options['only'] )) {
-	$hihi = "onCampaignDayOnly: false, ";
+	$jsopt = "onCampaignDayOnly: false, ";
 }
 if (isset( $options['once'] )) {
-	$hihi .= "showOnlyOnce: false, ";
+	$jsopt .= "showOnlyOnce: false, ";
 }
 if (isset( $options['coder'] )) {
-	$hihi .= "showCoders: true, ";
+	$jsopt .= "showCoders: true, ";
 }
 if (!empty($options['title'])) {
-	$hihi .= "title: '". $options['title']. "', ";
+	$jsopt .= "title: '". $options['title']. "', ";
 }
 if (!empty($options['big_text'])) {
-	$hihi .= "bigText: '". $options['big_text']. "', ";
+	$jsopt .= "bigText: '". $options['big_text']. "', ";
 }
 if (!empty($options['small_text'])) {
-	$hihi .= "smallText: '". $options['small_text']. "'";
+	$jsopt .= "smallText: '". $options['small_text']. "'";
+}if (!empty($options['count_text'])) {
+	$jsopt .= "countText: '". $options['count_text']. "', ";
+}
+if (!empty($options['close_text'])) {
+	$jsopt .= "closeText: '". $options['close_text']. "'";
 }
 
 ?>
 <script>
-copyrightCampaign({ <?php echo $hihi; ?> });
+copyrightCampaign({ <?php echo $jsopt; ?> });
 </script>
 <?php
 }
